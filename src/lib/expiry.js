880693cfdf75308c2197c374cdb6706d7d8dd085
Madelen Yoo <rolemadelen@tuta.io>
2024-02-15 21:24:35 -0500

    Code refactor
    Track UI code refactored
    
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- src/lib/expiry.js
+++ src/lib/expiry.js
@@ -1,6 +1,5 @@
 export const expiryInMinutes = () => {
   const now = new Date();
   const expiryInMins = 24 * 60; // 24 hours
-  // return now.getTime() + expiryInMins * 60 * 1000;
-  return now.getTime() + 100;
+  return now.getTime() + expiryInMins * 60 * 1000;
 };
