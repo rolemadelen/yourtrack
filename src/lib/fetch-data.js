6dc549c30ec4ac131385ed82105195e9c6f12d0f
rolemadelen <rolemadelen17@pm.me>
2024-02-15 21:44:20 -0500

    fetch data 'request' part separated

--- src/lib/fetch-data.js
+++ src/lib/fetch-data.js
@@ -1,35 +1,32 @@
+const request = (token) => ({
+  method: 'GET',
+  headers: { Authorization: `Bearer ${token}` },
+});
+
 export async function fetchTracks(token) {
-  console.log('[Calling fetchTracks]');
-  const request = {
-    method: 'GET',
-    headers: { Authorization: `Bearer ${token}` },
-  };
+  console.debug('[Calling fetchTracks]');
+  const req = request(token);
 
   const TOP_MONTH_TRACK_URL = `https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=10`;
-
-  const result = await fetch(TOP_MONTH_TRACK_URL, request);
+  const result = await fetch(TOP_MONTH_TRACK_URL, req);
   return await result.json();
 }
 
 export async function fetchAudioPreview(id, token) {
-  console.log('[Calling fetchAudioPreview]');
-  const result = await fetch(`https://api.spotify.com/v1/tracks/${id}`, {
-    method: 'GET',
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  console.debug('[Calling fetchAudioPreview]');
+  const req = request(token);
 
+  const AUDIO_PREVIEW_URL = `https://api.spotify.com/v1/tracks/${id}`;
+  const result = await fetch(AUDIO_PREVIEW_URL, req);
   return await result.json();
 }
 
 export async function fetchProfile(token) {
-  console.log('[Calling fetchProfile]');
-  const request = {
-    method: 'GET',
-    headers: { Authorization: `Bearer ${token}` },
-  };
+  console.debug('[Calling fetchProfile]');
+  const req = request(token);
 
   const USER_PROFILE_URL = 'https://api.spotify.com/v1/me';
-  const userData = await fetch(USER_PROFILE_URL, request);
+  const userData = await fetch(USER_PROFILE_URL, req);
 
   return await userData.json();
 }
