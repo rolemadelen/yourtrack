53ff013483a23ee3b3f97687300ec290c9132854
Madelen Yoo <rolemadelen@tuta.io>
2024-02-17 11:53:34 -0500

    code refactored
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- src/lib/expiry.js
+++ src/lib/expiry.js
@@ -3,3 +3,16 @@ export const expiryInMinutes = () => {
   const expiryInMins = 24 * 60; // 24 hours
   return now.getTime() + expiryInMins * 60 * 1000;
 };
+
+export function checkExpiry(tracks) {
+  const now = new Date();
+  tracks.items.forEach(({ id }) => {
+    const track = localStorage.getItem(id);
+    if (track) {
+      const expiry = JSON.parse(track).expiry;
+      if (now.getTime() > expiry) {
+        localStorage.removeItem(id);
+      }
+    }
+  });
+}
